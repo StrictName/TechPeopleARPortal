1e450f1ac35d6362237de60957f36e992943ad1a
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2023-04-16 21:42:24 -0600

    import collection and addDoc functions from Firestore

--- arportal/src/App.js
+++ arportal/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import firebaseConfig from "./firebaseconfig";
+import React, { useState } from "react";
+
+import { collection, addDoc } from "firebase/firestore"; // import collection and addDoc functions from Firestore
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [userId, setUserId] = useState("");
+  const [text, setText] = useState("");
+  const [pokemon, setPokemon] = useState(0);
 
-export default App;
+  const handleUserIdChange = (event) => {
+    setUserId(event.target.value);
+  };
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const add = async () => {
+    const usersRef = collection(firebaseConfig.db, "users");
+    const pokemonNum = Math.floor(Math.random() * 151);
+
+    try {
+      const docRef = await addDoc(usersRef, {
+        user_id: parseInt(userId),
+        text: text,
+        pokemon: pokemonNum,
+      });
+      console.log("Document written with ID: ", docRef.id);
+      setPokemon(pokemonNum);
+      console.log(pokemon);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+} //
