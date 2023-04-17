685afa1618735bff9bd9e3814e54c719ab66160f
StrictName <62078976+StrictName@users.noreply.github.com>
2023-04-16 21:45:56 -0600

    complete portal

--- arportal/src/App.js
+++ arportal/src/App.js
@@ -34,4 +34,32 @@ function App() {
       console.error("Error adding document: ", e);
     }
   };
-} //
+  
+  return (
+    <div>
+      <h1>TechPeopleAR</h1>
+      <input
+        value={userId}
+        onChange={handleUserIdChange}
+        placeholder="User Id"
+      />
+      <input value={text} onChange={handleTextChange} placeholder="Text" />
+
+      <button onClick={() => add()}>Send</button>
+      <div>
+        Given Pokemon: {pokemon}
+        {pokemon !== 0 ? (
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${String(
+              pokemon
+            )}.png`}
+          />
+        ) : (
+          <p>Send data to get Pokemon</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
